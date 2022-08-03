7aa1f06dd7c710521d356317b7f53e235e48a8f0
FourmiSushi <neko@fourmisushi.soy>
2022-08-04 02:29:10 +0900

    v0.2.0

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ mkdir -p gh-build/linux
 mkdir -p gh-build/windows
 mkdir -p gh-build/macos
 
-./dotnet6/dotnet publish . -r linux-x64 -c Release --sc -p:PublishSingleFile=true -o gh-build/linux
 ./dotnet6/dotnet publish . -r win-x64 -c Release --sc -p:PublishSingleFile=true -o gh-build/windows
+./dotnet6/dotnet publish . -r linux-x64 -c Release --sc -p:PublishSingleFile=true -o gh-build/linux
 ./dotnet6/dotnet publish . -r osx-x64 -c Release --sc -p:PublishSingleFile=true -o gh-build/macos
+
+mv gh-build/windows/filversish.exe gh-build/windows/filversish_win-x64.exe
+mv gh-build/linux/filversish gh-build/linux/filversish_linux-x64
+mv gh-build/macos/filversish gh-build/macos/filversish_osx-x64
